7d8d6e6c71a805a64be5f5d0ddb9494544dd7a4a
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-01 16:31:31 +0400

    Task 4

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -5,15 +5,15 @@
         static void Main(string[] args)
         {
             // Task 1
-            string Name = "Mohammed";
-            int Age = 23;
-            double hight = 175;
-            bool student = true;
+            //string Name = "Mohammed";
+            //int Age = 23;
+            //double hight = 175;
+            //bool student = true;
 
-            Console.WriteLine(Name);
-            Console.WriteLine(Age);
-            Console.WriteLine(hight);
-            Console.WriteLine(student);
+            //Console.WriteLine(Name);
+            //Console.WriteLine(Age);
+            //Console.WriteLine(hight);
+            //Console.WriteLine(student);
 
             // Task 2
             //Console.WriteLine("Enter the length");
@@ -29,19 +29,40 @@
             //Console.WriteLine("Perimeter: " + Perimeter);
 
             // Task 3
-            Console.WriteLine("Enter Number");
-            int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Number");
+            //int number = int.Parse(Console.ReadLine());
 
-            int CheckNumber = number % 2;
+            //int CheckNumber = number % 2;
 
-            if (CheckNumber == 0)
-            {
-                Console.WriteLine("Number is even");
-            }
-            else
-            {
-                Console.WriteLine("Number is odd");
-            }
+            //if (CheckNumber == 0)
+            //{
+            //    Console.WriteLine("Number is even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is odd");
+            //}
+
+            // Task 4
+            //Console.WriteLine("Enter Age");
+            //int Age = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Do you have a valid national ID (yes/no)?");
+            //string vID = (Console.ReadLine());
+
+            //bool yes = vID == "yes";
+            //bool no = vID == "no";
+
+            //if (Age >= 18 && yes)
+            //{
+            //    Console.WriteLine("You are eligible to vote");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are not eligible to vote");
+            //}
+
+
         }
     }
 }
