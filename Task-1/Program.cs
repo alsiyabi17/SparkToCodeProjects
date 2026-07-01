07c5a6ba6963766fb52556f9a00f877c9bd8c9a7
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-01 14:40:58 +0400

    Task 2

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -14,6 +14,19 @@
             Console.WriteLine(Age);
             Console.WriteLine(hight);
             Console.WriteLine(student);
+
+            // Task 2
+            Console.WriteLine("Enter the length");
+            float length = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width");
+            float width = float.Parse(Console.ReadLine());
+
+            float Area = length * width;
+            float Perimeter = 2 * (length + width);
+
+            Console.WriteLine("Area: " + Area);
+            Console.WriteLine("Perimeter: " + Perimeter);
         }
     }
 }
