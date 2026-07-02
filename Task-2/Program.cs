0280161ae76b1b0d2472cf34f047749c5a7cf778
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-02 11:39:39 +0400

    Task 3 - Multiplication Table

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -14,20 +14,31 @@
             //}
 
             //Console.WriteLine("Liftoff!");
-
+            //////////////////////////////////////////////////////////////////////////////////////////////////
 
             // Task 2 - Sum of Numbers 1 to N
-            Console.WriteLine("Enter a positive whole number:");
-            int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a positive whole number:");
+            //int n = int.Parse(Console.ReadLine());
 
-            int sum = 0;
+            //int sum = 0;
 
-            for (int i = 1; i <= n; i++)
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    sum = sum + i;
+            //}
+
+            //Console.WriteLine("The sum is: " + sum);
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // Task 3 - Multiplication Table
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
             {
-                sum = sum + i;
+                Console.WriteLine(number + " x " + i + " = " + (number * i));
             }
 
-            Console.WriteLine("The sum is: " + sum);
 
         }
     }
