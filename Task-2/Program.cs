2ff1659f81c122284da646fb0f89037d4678a5b9
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-02 11:34:41 +0400

    Task 1 - Countdown Timer

---
+++ Task-2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the starting number:");
+            int start = int.Parse(Console.ReadLine());
+
+            for (int i = start; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Liftoff!");
+        }
+    }
+}
