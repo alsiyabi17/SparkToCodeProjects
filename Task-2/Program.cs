7c661c3ea8f0d7296d0756cf2fb26291b9ece129
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-02 11:37:26 +0400

    Task 2 - Sum of Numbers 1 to N

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -4,15 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the starting number:");
-            int start = int.Parse(Console.ReadLine());
+            // Task 1 - Countdown Timer
+            //Console.WriteLine("Enter the starting number:");
+            //int start = int.Parse(Console.ReadLine());
 
-            for (int i = start; i >= 1; i--)
+            //for (int i = start; i >= 1; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine("Liftoff!");
+
+
+            // Task 2 - Sum of Numbers 1 to N
+            Console.WriteLine("Enter a positive whole number:");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine(i);
+                sum = sum + i;
             }
 
-            Console.WriteLine("Liftoff!");
+            Console.WriteLine("The sum is: " + sum);
+
         }
     }
 }
