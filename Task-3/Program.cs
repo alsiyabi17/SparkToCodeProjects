31f1e2a34acdead69ad8015d40f777a27eda6d00
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-05 15:02:41 +0400

    Task 1 - Absolute Difference

--- Task-3/Program.cs
+++ Task-3/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1 - Absolute Difference
+           
+            Console.WriteLine("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double Result = Math.Abs(num2 - num1);
+            Console.WriteLine(Result);
+
         }
     }
 }
