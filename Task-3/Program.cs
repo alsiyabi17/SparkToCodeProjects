253c36b6c18f5253d2f29cd701be55905ec4f12d
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-05 15:05:45 +0400

    Task 2 - Power & Root Explorer

--- Task-3/Program.cs
+++ Task-3/Program.cs
@@ -5,16 +5,24 @@
         static void Main(string[] args)
         {
             // Task 1 - Absolute Difference
-           
-            Console.WriteLine("Enter the first number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter the first number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter the second number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
 
-            double Result = Math.Abs(num2 - num1);
-            Console.WriteLine(Result);
+            //double Result = Math.Abs(num2 - num1);
+            //Console.WriteLine(Result);
 
+            // Task 2 - Power & Root Explorer
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int square = (int)Math.Pow(num, 2);
+            int squareRoot = (int)Math.Sqrt(num);
+
+            Console.WriteLine("The square of " + num + " is: " + square);
+            Console.WriteLine("The square root of " + num + " is: " + squareRoot);
         }
     }
 }
