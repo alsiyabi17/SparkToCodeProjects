a4f4bdbd8efcfe85226ed62652751acbaffa4ef8
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-05 15:10:03 +0400

    Task 3 - Name Formatter

--- Task-3/Program.cs
+++ Task-3/Program.cs
@@ -13,16 +13,29 @@
 
             //double Result = Math.Abs(num2 - num1);
             //Console.WriteLine(Result);
-
+            // -----------------------------------------------------------------------------
             // Task 2 - Power & Root Explorer
-            Console.WriteLine("Enter a number: ");
-            int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter a number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
 
-            int square = (int)Math.Pow(num, 2);
-            int squareRoot = (int)Math.Sqrt(num);
+            //int square = (int)Math.Pow(num, 2);
+            //int squareRoot = (int)Math.Sqrt(num);
 
-            Console.WriteLine("The square of " + num + " is: " + square);
-            Console.WriteLine("The square root of " + num + " is: " + squareRoot);
+            //Console.WriteLine("The square of " + num + " is: " + square);
+            //Console.WriteLine("The square root of " + num + " is: " + squareRoot);
+            // -----------------------------------------------------------------------------
+            // Task 3 - Name Formatter
+            Console.WriteLine("Enter your full name: ");
+            string fullName = Console.ReadLine();
+
+            string nameUpper = fullName.ToUpper();
+            string nameLower = fullName.ToLower();
+            string nameLength = fullName.Length.ToString();
+
+            Console.WriteLine("Your name in uppercase: " + nameUpper);
+            Console.WriteLine("Your name in lowercase: " + nameLower);
+            Console.WriteLine("The length of your name is: " + nameLength);
+
         }
     }
 }
