3e04249d1ffb57ee0ca93b0c8dab0add435ee44a
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-07 02:36:26 +0400

    Task 1 - Personalized Welcome Function

---
+++ Task-5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void PrintWelcome(string name)
+        {
+            Console.WriteLine("Welcome, " + name + "!");
+            Console.WriteLine("We're glad to have you here.");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            PrintWelcome(name);
+        }
+    }
+}
