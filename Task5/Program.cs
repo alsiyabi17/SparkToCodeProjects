0816165e3ebf1d803b1fca94ca0a1bd5339d08bf
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-07 22:42:05 +0400

    Task 2 - Dynamic To-Do List

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -5,16 +5,35 @@
         static void Main(string[] args)
         {
             // Task 1 - Fixed Grades Array
-            int[] grades = new int[5];
-            for (int i = 0; i < grades.Length; i++)
+            //int[] grades = new int[5];
+            //for (int i = 0; i < grades.Length; i++)
+            //{
+            //    Console.Write("Enter grade " + (i + 1) + ": ");
+            //    grades[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("\nStudent Grades:");
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+            // -------------------------------------------------------------
+
+            // Task 2 - Dynamic To-Do List
+            List<string> todoList = new List<string>();
+
+            for (int i = 0; i < 5; i++)
             {
-                Console.Write("Enter grade " + (i + 1) + ": ");
-                grades[i] = int.Parse(Console.ReadLine());
+                Console.Write("Enter task " + (i + 1) + ": ");
+                todoList.Add(Console.ReadLine());
             }
-            Console.WriteLine("\nStudent Grades:");
-            foreach (int grade in grades)
+
+            Console.WriteLine("\nTo-Do List:");
+            int taskNumber = 1;
+
+            foreach (string task in todoList)
             {
-                Console.WriteLine(grade);
+                Console.WriteLine(taskNumber + ". " + task);
+                taskNumber++;
             }
         }
     }
