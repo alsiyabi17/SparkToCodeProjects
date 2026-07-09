8ac82d760797ba2f3266a288f945e315654337a5
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-09 12:19:03 +0400

    creating file

---
+++ Mini Compound Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini_Compound_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
