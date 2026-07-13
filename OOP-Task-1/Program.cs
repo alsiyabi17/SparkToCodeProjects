c5307af606f8cf1731d4f5d4b62180d3cb1bc4af
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-13 13:04:47 +0400

    create OOP-Task1 file

---
+++ OOP-Task-1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
