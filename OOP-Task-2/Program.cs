a7608b0e1c83178052133233c1bc064dfa8ebd2d
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-16 14:47:54 +0400

    System Lists And Main Menu Blueprint

--- OOP-Task-2/Program.cs
+++ OOP-Task-2/Program.cs
@@ -61,6 +61,43 @@
     {
         static void Main(string[] args)
         {
+            List<Room> rooms = new List<Romm>();
+            List<Guest> guests = new List<Guest>();
+
+            rooms.Add(new Room(101, "Single", 100, true));
+            rooms.Add(new Room(101, "Single", 100, false));
+            rooms.Add(new Room(102, "Double", 150, true));
+            rooms.Add(new Room(102, "Double", 150, false));
+            rooms.Add(new Room(103, "Suite", 200, true));
+            rooms.Add(new Room(103, "Suite", 200, false));
+
+            bool exit = false;
+
+            while (exit == false)
+            {
+                Console.WriteLine("================================================");
+                Console.WriteLine("GRAND VISTA HOTEL — MANAGEMENT SYSTEM");
+                Console.WriteLine("================================================");
+                Console.WriteLine("1. Add New Room");
+                Console.WriteLine("2. Register New Guest");
+                Console.WriteLine("3. Book a Room for a Guest");
+                Console.WriteLine("4. View All Rooms");
+                Console.WriteLine("5. View All Guest");
+                Console.WriteLine("6. Search & Filter Rooms");
+                Console.WriteLine("7. Guest & Booking Statistics");
+                Console.WriteLine("8. Update Room Price");
+                Console.WriteLine("9. Guest Lookup by Name");
+                Console.WriteLine("10. Room Type Breakdown Report");
+                Console.WriteLine("11. Check Out a Guest\r\n");
+                Console.WriteLine("12. Remove Unavailable Rooms");
+                Console.WriteLine("13. Extend Guest Stay");
+                Console.WriteLine("14. Highest Revenue Booking");
+                Console.WriteLine("15. Guest Pagination Viewer");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("================================================");
+                Console.WriteLine("Enter your choice: ");
+            }
+            
 
 
         }
