f8db466d0d7509303be9f9b4594fa60b97fdaa93
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-25 23:31:14 +0400

    Add employee registration feature
    Implement employee registration functionality in the Main method. Users can now input employee details (name, salary, and age) which are then saved to the database using Entity Framework Core. Added using statement for EFCoreProject.Models namespace.

--- EFCoreProject/Program.cs
+++ EFCoreProject/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace EFCoreProject
+﻿using EFCoreProject.Models;
+
+namespace EFCoreProject
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProjectContext context = new ProjectContext();
+
+            // add data on table employee
+            //Employee e1 = new Employee();
+            //e1.EmployeeName = "Mohammed";
+            //e1.EmployeeSalary = 1000;
+            //e1.EmployeeAge = 23;
+            //context.Employees.Add(e1);
+            //context.SaveChanges();
+
+
+            // case 1 register employee
+            Console.WriteLine("Register employee");
+
+            Employee e1 = new Employee();
+
+            Console.WriteLine("Enter Employee Name");
+            e1.EmployeeName = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Salary");
+            e1.EmployeeSalary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Age");
+            e1.EmployeeAge = Convert.ToInt32(Console.ReadLine());
+
+            context.Employees.Add(e1);
+            context.SaveChanges();
         }
     }
 }
