693a1c9112cee3880cd6aa8ab6e2e4ca769c2326
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-26 10:47:06 +0400

    Add DeptLocations model and update workspace
    Adds a new EF Core model EFCoreProject/Models/DeptLocations.cs defining a DeptLocations entity with a composite primary key (DepartmentId, DepartmentLocation) and a navigation property to Department. Also updates IDE metadata (.idea/.../workspace.xml) to include the new file in the changelist and highlighting settings.

---
+++ EFCoreProject/Models/DeptLocations.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreProject.Models;
+
+[PrimaryKey(nameof(DepartmentId), nameof(DepartmentLocation))]
+
+public class DeptLocations
+{
+    [ForeignKey(Dept)]
+    public int DepartmentId { get; set; }
+    public Department Dept { get; set; }
+    
+    
+    public string DepartmentLocation { get; set; }
+}
