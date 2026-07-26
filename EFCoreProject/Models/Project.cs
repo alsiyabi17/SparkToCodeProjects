6622cc6d220ee4a16ba0905e6ba6b17d5b61901a
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-26 10:35:13 +0400

    Add Project model class
    Create a new Project entity model for the EFCoreProject with properties for ProjectId, ProjectName, and ProjectLocation. Include a navigation property for the relationship with employees through the empProject collection.

---
+++ EFCoreProject/Models/Project.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreProject.Models;
+
+public class Project
+{
+    [Key]
+    public int ProjectId { get; set; }
+    public string ProjectName { get; set; }
+    public string ProjectLocation { get; set; }
+    
+    // Public List<Employee> Employees { get; set; }
+    
+    public List<empProject> empProject { get; set; }
+}
