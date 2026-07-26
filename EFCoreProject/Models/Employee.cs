aab924901ec79e9b7fd452882b77dcf732c7271b
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-26 10:32:33 +0400

    Add EF Core relationships to Employee model
    Add navigation properties and data annotations to the Employee model to support entity framework relationships: department associations (1-M), department management (inverse), dependent (1-1), project assignments via join table (M-M), and employee supervision hierarchy (self-referencing).

--- EFCoreProject/Models/Employee.cs
+++ EFCoreProject/Models/Employee.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace EFCoreProject.Models
 {
     public class Employee
     {
+        [Key]
         public int EmployeeId { get; set; }
         public string EmployeeName { get; set; }
         public int EmployeeAge { get; set; }
         public double EmployeeSalary { get; set; }
 
+        
+        
+        //WorkFor 1 - M
+        [ForeignKey("D")]
+        public int DepartmentId { get; set; }
+        public Department D { get; set; }
+        
+        //Manage
+        [InverseProperty("Employee")]
+        public Department ManageDepart { get; set; }
+        
+        //Dependent 1 - 1
+        public Dependent Dependent { get; set; }
+        
+        //WorkOn M - M
+        // public List<Project> projects { get; set; }
+        public List<empProj> empProjs { get; set; }
+        
+        //Supervision
+        [InverseProperty("supervisor")]
+        public List<Employee> supervisee { get; set; }
+        
+        [ForeignKey("Supervisor")]
+        public int SupervisorId { get; set; }
+        public Employee Supervisor { get; set; }
+        
     }
 }
