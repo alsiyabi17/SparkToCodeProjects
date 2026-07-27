e3cb58c97b73231377cb65ee2b28bba534df0655
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-07-27 20:36:38 +0400

    Add ProjectContext DbContext for e-commerce
    Create ProjectContext class that inherits from DbContext to manage entity models for the e-commerce database. Includes DbSet properties for Product, Order, Review, Category, User, and OrderProduct entities. Configures SQL Server connection string for the CompanyProjectDb database.

---
+++ E-CommerceDatabseTask/ProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using E_CommerceDatabseTask.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_CommerceDatabseTask
+{
+    public class ProjectContext
+    {
+        // Register Models
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Review { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        // 2- Configure database connection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Mohammed\\SQLEXPRESS;Database=CompanyProjectDb;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+    }
+}
